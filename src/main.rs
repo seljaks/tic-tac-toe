fd9612409a2d884741f97207b66e6ad61c944793
seljaks <33955366+seljaks@users.noreply.github.com>
2024-12-23 14:51:12 +0100

    method all the things

--- src/main.rs
+++ src/main.rs
@@ -16,10 +16,19 @@ enum Tile {
 }
 
 #[derive(Debug)]
+enum GameState {
+    Ongoing,
+    Draw,
+    WinX,
+    WinO,
+}
+
+#[derive(Debug)]
 struct Game {
     board: [Tile; 9],
     moves: [usize; 9],
     active_player: Player,
+    state: GameState,
 }
 
 impl Game {
@@ -27,7 +36,8 @@ impl Game {
         Game {
             board: [Tile::EMPTY; 9],
             moves: [0, 1, 2, 3, 4, 5, 6, 7, 8],
-            active_player: Player::X,
+            active_player: Player::O,
+            state: GameState::Ongoing,
         }
     }
     fn get_possible_moves(&self) -> Vec<&str> {
@@ -38,8 +48,80 @@ impl Game {
             .map(|(_, c)| MOVES[c])
             .collect()
     }
+    fn update_state(&mut self) -> () {
+        self.state = if self.is_win_x() {
+            GameState::WinX
+        } else if self.is_win_o() {
+            GameState::WinO
+        } else if self.is_draw() {
+            GameState::Draw
+        } else {
+            GameState::Ongoing
+        }
+    }
+    fn check_state(&mut self) -> () {
+        match self.state {
+            GameState::WinX => {
+                println!("Player using x won!");
+                println!("Final board: {}", &self);
+            }
+            GameState::WinO => {
+                println!("Player using o won!");
+                println!("Final board: {}", &self)
+            }
+            GameState::Draw => {
+                println!("Game is drawn.");
+                println!("Final board: {}", &self)
+            }
+            GameState::Ongoing => {
+                self.active_player = match self.active_player {
+                    Player::X => Player::O,
+                    Player::O => Player::X,
+                };
+                self.get_user_input();
+            }
+        }
+    }
+    fn get_user_input(&mut self) -> () {
+        println!("{}", self);
+        let possible_moves = &self.get_possible_moves();
+        println!("Possible moves: {:?}", possible_moves);
+        let input = loop {
+            let mut input = String::new();
+            let _ = io::stdin().read_line(&mut input);
+            let input = if possible_moves.contains(&input.trim()) {
+                input
+            } else {
+                println!("Not a correct entry, try again!");
+                continue;
+            };
+            break input;
+        };
+        let x_or_o = match self.active_player {
+            Player::X => Tile::X,
+            Player::O => Tile::O,
+        };
+        match input.trim() {
+            "1" => self.board[0] = x_or_o,
+            "2" => self.board[1] = x_or_o,
+            "3" => self.board[2] = x_or_o,
+            "4" => self.board[3] = x_or_o,
+            "5" => self.board[4] = x_or_o,
+            "6" => self.board[5] = x_or_o,
+            "7" => self.board[6] = x_or_o,
+            "8" => self.board[7] = x_or_o,
+            "9" => self.board[8] = x_or_o,
+            _ => unreachable!(),
+        };
+    }
+    fn get_best_move(&self) -> usize {
+        0
+    }
     fn is_over(&self) -> bool {
-        self.is_win_x() | self.is_win_o() | self.is_draw()
+        matches!(
+            self.state,
+            GameState::Draw | GameState::WinX | GameState::WinO
+        )
     }
     fn is_win_x(&self) -> bool {
         match self.board {
@@ -106,54 +188,9 @@ impl Display for Game {
 
 fn main() {
     let mut game = Game::new();
-    loop {
-        println!("{}", &game);
-        let possible_moves = game.get_possible_moves();
-        println!("Possible moves: {:?}", &possible_moves);
-        let input = loop {
-            let mut input = String::new();
-            let _ = io::stdin().read_line(&mut input);
-            let input = if possible_moves.contains(&input.trim()) {
-                input
-            } else {
-                println!("Not a correct entry, try again!");
-                continue;
-            };
-            break input;
-        };
-        let x_or_o = match game.active_player {
-            Player::X => Tile::X,
-            Player::O => Tile::O,
-        };
-        match input.trim() {
-            "1" => game.board[0] = x_or_o,
-            "2" => game.board[1] = x_or_o,
-            "3" => game.board[2] = x_or_o,
-            "4" => game.board[3] = x_or_o,
-            "5" => game.board[4] = x_or_o,
-            "6" => game.board[5] = x_or_o,
-            "7" => game.board[6] = x_or_o,
-            "8" => game.board[7] = x_or_o,
-            "9" => game.board[8] = x_or_o,
-            _ => unreachable!(),
-        };
-        if game.is_draw() {
-            println!("Game is a draw!");
-            break;
-        } else if game.is_win_x() {
-            println!("Player {:?} won!", Player::X);
-            println!("Final position: {}", &game);
-            break;
-        } else if game.is_win_o() {
-            println!("Player {:?} won!", Player::O);
-            println!("Final position: {}", &game);
-            break;
-        } else {
-            game.active_player = match game.active_player {
-                Player::X => Player::O,
-                Player::O => Player::X,
-            }
-        }
+    while !game.is_over() {
+        game.check_state();
+        game.update_state();
     }
-    //println!("Game is over! Final position: {}", &game);
+    game.check_state();
 }
